f4d68c3ef6b63ab3c46d5925463077aa2eb5e40a
sorongosdev <dosora9@gmail.com>
2024-04-28 02:24:45 +0900

    윈도우 실행을 위한 세팅
    -파이썬 경로 설정
    -그레이들 다운그레이드

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,9 @@ android {
 
 chaquopy {
     defaultConfig {
-        buildPython("/usr/bin/python3")
+//        buildPython("/usr/bin/python3")
+        buildPython("C:/Users/Admin/AppData/Local/Programs/Python/Python312/python.exe")
+
         pip {
             install("numpy")
             install("matplotlib")
@@ -70,10 +72,10 @@ chaquopy {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -85,7 +87,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
