ea3f198b3d50e7bc87a8e07bde25e71035d861e2
sorongosdev <dosora9@gmail.com>
2024-05-12 11:28:28 +0900

    코드 정리, 4.8초 이후 녹음 중지 구현

--- app/src/main/java/com/example/tarsos_example/MainActivity.kt
+++ app/src/main/java/com/example/tarsos_example/MainActivity.kt
@@ -33,6 +33,7 @@ import com.chaquo.python.Python
 import com.chaquo.python.android.AndroidPlatform
 import java.io.File
 import java.io.IOException
+import java.util.Date
 
 var tarsosDSPAudioFormat: TarsosDSPAudioFormat? = null
 
@@ -49,46 +50,8 @@ class MainActivity : ComponentActivity() {
         if (!Python.isStarted()) {
             Python.start(AndroidPlatform(applicationContext));
         }
-// example.py를 읽음
-//        val py = Python.getInstance()
-//        val pyObj = py.getModule("example")
-
-        ///////////////////////////////////////////////////////////
-//        val waveBytes = readAssetFile(this, "Dm_AcusticPlug26_1.wav")
-//        val result = waveBytes?.let { bytes ->
-//            pyObj.callAttr("read_wav_file", bytes)
-//        }
-        ///////////////////////////////////////////////////////////
-
-
-        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//        // 내부 저장소에 있는 파일의 경로를 지정합니다.
-//        val filePath = "${filesDir.absolutePath}/recorded_audio.wav"
-//
-//        // 정의한 함수를 사용하여 파일에서 바이트 데이터를 읽습니다.
-//        val waveBytes = readFileBytes(filePath)
-//
-//        // 읽은 바이트 데이터를 Python 코드에 전달합니다.
-//        val result = waveBytes?.let { bytes ->
-//            pyObj.callAttr("read_wav_file", bytes)
-//        }
-        //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-        // Chaquopy에서 파이썬 함수 호출 결과를 받음
-//        val (sampleRate, signalList) = result?.asList() ?: listOf(0, listOf<Float>())
-
-        // 결과 로그 출력
-        Log.d("WAV_INFO", "Sample Rate: $sampleRate")
-        // 신호 배열의 일부를 로그로 출력하려면, signalList를 적절히 슬라이싱 하거나 요약하여 출력
-//        Log.d("WAV_INFO", "Signal Array First Elements: ${signalList}")
-
-
-        // 메인에서 온 로그를 찍어주는 부분
-        // val logMessage = pyObj.callAttr("main").toString()
-
-        // Logcat에 로그 메시지 출력
-        // Log.d("PythonLog", logMessage)
+        val py = Python.getInstance()
+        val pyObj = py.getModule("example")
 
         setContent {
             Tarsos_exampleTheme {
@@ -118,6 +81,7 @@ class MainActivity : ComponentActivity() {
             null
         }
     }
+
     fun readAssetFile(context: Context, fileName: String): ByteArray? {
         return try {
             context.assets.open(fileName).use { inputStream ->
