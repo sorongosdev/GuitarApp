ceb1d9c79c665d9c29f9be66f1ac8c3b0ac01fec
jessy <yaeunjess@gmail.com>
2024-05-12 18:49:41 +0900

    박자 알고리즘 완료

--- app/src/main/python/example.py
+++ app/src/main/python/example.py
@@ -28,19 +28,19 @@ TWELVE_ROOT_OF_2 = math.pow(2, 1.0 / 12)
 
 ## wav 파일 읽은 후, sample_rate와 input_buffer 반환
 # (sample_rate : int, input_buffer : NDArray[Any]) 반환, NDArray[Any]는 실수값의 numpy 배열을 의미
-def read_wav_file(path, filename):
-    # Reads the input WAV file from HDD disc.
-    wav_handler = wave.open(path + filename,'rb')    # 지정된 경로에 wav 파일을 읽기 전용 모드로 연다.
-    num_frames = wav_handler.getnframes()            # 파일에서 sample의 총 개수를 얻는다. 44100*(wav 길이 예로 4초) = 176400개
-    sample_rate = wav_handler.getframerate()         # 파일의 sample_rate를 얻는다.
-                                                     # sampling rate는 초당 샘플링 횟수를 의미하며, 단위는 Hz이다. wav파일의 헤더 부분에 이 정보가 포함되어 있다.
-    wav_frames = wav_handler.readframes(num_frames)  # 모든 frame을 읽는다. wav_frames는 num_frames의 두 배이다. 각 샘플이 2바이트로 표현되기 때문. wav_frames의 바이트 배열 길이는 176400*2 = 352800 바이트이다.
+def read_wav_file(wave_bytes):
+    wave_file = io.BytesIO(wave_bytes)
+    with wave.open(wave_file, 'rb') as wf:
+        params = wf.getparams()
+        frames = wf.readframes(params.nframes)
 
-    # Loads the file into a NumPy contiguous array.
-    # WAV 파일의 프레임을 numpy 배열로 변환합니다.
-    # Convert Int16 into float64 in the range of [-1, 1].
-    # This means that the sound pressure values are mapped to integer values that can range from -2^15 to (2^15)-1.
-    # We can convert our sound array to floating point values ranging from -1 to 1 as follows.
+#     wav_handler = wave.open(path + filename,'rb')    # 지정된 경로에 wav 파일을 읽기 전용 모드로 연다.
+#     num_frames = wav_handler.getnframes()            # 파일에서 sample의 총 개수를 얻는다. 44100*(wav 길이 예로 4초) = 176400개
+#     sample_rate = wav_handler.getframerate()         # 파일의 sample_rate를 얻는다. 44100
+    sample_rate = 44100
+#     wav_frames = wav_handler.readframes(num_frames)  # 모든 frame을 읽는다. wav_frames는 num_frames의 두 배이다. 각 샘플이 2바이트로 표현되기 때문. wav_frames의 바이트 배열 길이는 176400*2 = 352800 바이트이다.
+    wav_frames = frames
+
     signal_temp = np.frombuffer(wav_frames, np.int16) # 읽은 wav_frame 데이터를 numpy 배열로 변환한다. 데이터 타입은 int16이다.
     signal_array = np.zeros(len(signal_temp), float) # wav_frames로부터 생성된 numpy 배열이다. 신호를 저장할 float 타입의 numpy 배열을 생성한다.
 
@@ -48,7 +48,7 @@ def read_wav_file(path, filename):
         signal_array[i] = signal_temp[i] / (2.0**15) # int16 타입의 값을 [-1, 1] 범위의 float64 타입으로 변환합니다.
 
     print("------------------------------")
-    print("file_name: " + str(filename))
+#     print("file_name: " + str(filename))
     print("sample_rate: " + str(sample_rate) + " Hz")
     print("input_buffer.size(sample의 총 갯수): " + str(len(signal_array)) + " 개")
     print("seconds(input_buffer.size/sample_rate): " + to_str_f4(len(signal_array)/sample_rate) + " s")
@@ -556,7 +556,7 @@ def to_str_f4(value):
     return "{0:.4f}".format(value)
 
 
-def main():
+def main(wave_bytes):
     print("\nPolyphonic note detector\n")
 
     unique_notes = get_unique_key()
@@ -564,7 +564,7 @@ def main():
     guitar_chords_notes = get_all_guitar_chords_notes()
     ordered_note_freq = get_all_notes_freq()
 
-    sample_rate_file, input_buffer = read_wav_file(path, filename)
+    sample_rate_file, input_buffer = read_wav_file(wave_bytes)
     buffer_chunks = divide_buffer_into_non_overlapping_chunks(input_buffer, fft_len)
 
     all_top_results = [] # [(chunk_num, freq, value, note_name), ...]
