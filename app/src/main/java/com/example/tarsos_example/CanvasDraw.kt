1dbeb5279f2afda047f609ec6d716810564b5133
Sorongos <dosora9@gmail.com>
2024-05-13 14:30:33 +0900

    더미 피드백 노트 리스트로 한 마디만 음표 빨간색으로 그리기

--- app/src/main/java/com/example/tarsos_example/CanvasDraw.kt
+++ app/src/main/java/com/example/tarsos_example/CanvasDraw.kt
@@ -54,14 +54,14 @@ fun DrawNotes(noteType: List<Int>, location: Int, modifier: Modifier = Modifier)
 
         // for 문 간소화
         listOf(1, 4, 7, 10).forEach { i ->
-            val noteIndex = (i-1)/3 // { 0,1 / 1,4 / 2,7 / 3,10 } =======> { 3n+1 = i }
+            val noteIndex = (i - 1) / 3 // { 0,1 / 1,4 / 2,7 / 3,10 } =======> { 3n+1 = i }
             if (noteType[noteIndex] == 1) {
                 val xOffset = startX_measure + i * (measure_width / 13) // 음표가 그려지는 곳
 
                 drawLine(
                     color = Color.Black,
-                    start = Offset(x = xOffset + lineLength/2, y = startY_tail),
-                    end = Offset(x = xOffset + lineLength/2, y = endY_tail-lineLength/2),
+                    start = Offset(x = xOffset + lineLength / 2, y = startY_tail),
+                    end = Offset(x = xOffset + lineLength / 2, y = endY_tail - lineLength / 2),
                     strokeWidth = Stroke.DefaultMiter
                 )
 
@@ -70,6 +70,45 @@ fun DrawNotes(noteType: List<Int>, location: Int, modifier: Modifier = Modifier)
                 val end = Offset(x = xOffset + lineLength / 2, y = centerY - lineLength / 2)
                 drawLine(
                     color = Color.Black,
+                    start = start,
+                    end = end,
+                    strokeWidth = Stroke.DefaultMiter
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DrawFeedBackNotes(feedbackNoteList: List<Int>, location: Int, modifier: Modifier) {
+    Canvas(modifier = modifier) {
+        val startX_measure = if (location == 1) {
+            1 * (size.width / 10) // 첫번째 마디 시작점
+        } else {
+            5 * (size.width / 10) // 두번째 마디 시작점
+        }
+        val measure_width = 4 * (size.width / 10) // 한 마디 넓이
+        val startY_tail = size.height * 1 / 5 // 꼬리가 시작되는 Y지점
+        val endY_tail = size.height * 4 / 5 // 꼬리가 끝나는 Y지점
+        val centerY = size.height * 4 / 5 // 선의 중심점
+        val lineLength = sqrt(2f) * 25f // 45도 각도에서의 선 길이, 대각선 길이 계산
+
+        for (i in 1..12) {
+            if (feedbackNoteList[i] == 1) {
+                val xOffset = startX_measure + i * (measure_width / 13) // 음표가 그려지는 곳
+
+                drawLine(
+                    color = Color.Red,
+                    start = Offset(x = xOffset + lineLength / 2, y = startY_tail),
+                    end = Offset(x = xOffset + lineLength / 2, y = endY_tail - lineLength / 2),
+                    strokeWidth = Stroke.DefaultMiter
+                )
+
+                // 45도 기울인 선을 그리기 위한 시작점과 끝점 계산
+                val start = Offset(x = xOffset - lineLength / 2, y = centerY + lineLength / 2)
+                val end = Offset(x = xOffset + lineLength / 2, y = centerY - lineLength / 2)
+                drawLine(
+                    color = Color.Red,
                     start = start,
                     end = end,
                     strokeWidth = Stroke.DefaultMiter
---
+++ app/src/main/java/com/example/tarsos_example/consts/NoteTypes.kt
@@ -0,0 +1,16 @@
+package com.example.tarsos_example.consts
+
+/**악보 타입*/
+object NoteTypes {
+    val note_1111 = listOf<Int>(1,1,1,1)
+    val note_1011 = listOf<Int>(1,0,1,1)
+    val note_1010 = listOf<Int>(1,0,1,0)
+
+    val note_feedback = listOf<Int>(
+        1,1,0,1,0, // 0~4
+        1,0,1,1,1, // 5~9
+        1,1,1,1,1, // 10~14
+        1,1,1,1,1, // 15~19
+        1,1,1,1,1, // 20~24
+    )
+}
