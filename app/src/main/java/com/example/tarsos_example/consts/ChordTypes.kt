14be9b9bf33aa561154f2f45824dc9a3bf0b9572
Sorongos <dosora9@gmail.com>
2024-05-15 16:26:05 +0900

    기타 코드 관련 const 파일 추가

---
+++ app/src/main/java/com/example/tarsos_example/consts/ChordTypes.kt
@@ -0,0 +1,13 @@
+package com.example.tarsos_example.consts
+
+object ChordTypes {
+    val chords_numbers: Map<Int, String> = mapOf(
+        1 to "A", 2 to "Am", 3 to "A7",
+        4 to "B", 5 to "Bm", 6 to "B7",
+        7 to "C", 8 to "C7",
+        9 to "D", 10 to "Dm", 11 to "D7",
+        12 to "E", 13 to "Em", 14 to "E7",
+        15 to "F", 16 to "Fm", 17 to "F7",
+        18 to "G", 19 to "G7"
+    )
+}
