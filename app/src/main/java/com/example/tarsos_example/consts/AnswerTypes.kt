0ac1c32c054b589741c109cbdc28401f80ea4259
sorongosdev <dosora9@gmail.com>
2024-05-28 20:15:54 +0900

    그레이 지우고 색 기획대로 수정

--- app/src/main/java/com/example/tarsos_example/consts/AnswerTypes.kt
+++ app/src/main/java/com/example/tarsos_example/consts/AnswerTypes.kt
@@ -12,12 +12,19 @@ object AnswerTypes {
     const val DELETED = 7
 
     val answerCodeMap: Map<Int, Color> = mapOf(
-        BEAT_W_CHORD_X to (Color.Red),       // 박자 X 코드 미정
-        BEAT_C_CHORD_X to (Color.Green),     // 박자 0 코드 미정
-        BEAT_W_CHORD_C to (Color.Blue),      // 박자 X 코드 0
-        BEAT_W_CHORD_W to (Color.Cyan),      // 박자 X 코드 X
-        BEAT_C_CHORD_C to (Color.Magenta),   // 박자 0 코드 0
-        BEAT_C_CHORD_W to (Color.Yellow),     // 박자 0 코드 X
-        DELETED to (Color.Gray)
+//        BEAT_W_CHORD_X to (Color.Red),       // 박자 X 코드 미정
+//        BEAT_C_CHORD_X to (Color.Green),     // 박자 0 코드 미정
+//        BEAT_W_CHORD_C to (Color.Blue),      // 박자 X 코드 0
+//        BEAT_W_CHORD_W to (Color.Cyan),      // 박자 X 코드 X
+//        BEAT_C_CHORD_C to (Color.Magenta),   // 박자 0 코드 0
+//        BEAT_C_CHORD_W to (Color.Yellow),     // 박자 0 코드 X
+//        DELETED to (Color.Gray)
+        BEAT_W_CHORD_X to (Color.Transparent),       // 박자 X 코드 미정
+        BEAT_C_CHORD_X to (Color.Transparent),     // 박자 0 코드 미정
+        BEAT_W_CHORD_C to (Color.Green),      // 박자 X 코드 0
+        BEAT_W_CHORD_W to (Color.Red),      // 박자 X 코드 X
+        BEAT_C_CHORD_C to (Color.Green),   // 박자 0 코드 0
+        BEAT_C_CHORD_W to (Color.Red),     // 박자 0 코드 X
+        DELETED to (Color.Transparent)
     )
 }
