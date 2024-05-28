257b3009ba517a1bf2280e7ede6cd8f29d8ce599
jessy <yaeunjess@gmail.com>
2024-05-29 00:18:12 +0900

    Bm코드 없애기

--- app/src/main/java/com/example/tarsos_example/consts/ChordTypes.kt
+++ app/src/main/java/com/example/tarsos_example/consts/ChordTypes.kt
@@ -3,21 +3,21 @@ package com.example.tarsos_example.consts
 object ChordTypes {
     val chords_int_string_map: Map<Int, String> = mapOf(
         1 to "A",
-        2 to "B", 3 to "Bm", 4 to "B7",
-        5 to "C", 6 to "C7",
-        7 to "Dm",
-        8 to "E",
-        9 to "F",
-        10 to "G", 11 to "G7"
+        2 to "B", 3 to "B7",
+        4 to "C", 5 to "C7",
+        6 to "Dm",
+        7 to "E",
+        8 to "F",
+        9 to "G", 10 to "G7"
     )
     val chords_string_int_map: Map<String, Int> = mapOf(
         "A" to 1,
-        "B" to 2, "Bm" to 3, "B7" to 4,
-        "C" to 5, "C7" to 6,
-        "Dm" to 7,
-        "E" to 8,
-        "F" to 9,
-        "G" to 10, "G7" to 11
+        "B" to 2, "B7" to 3,
+        "C" to 4, "C7" to 5,
+        "Dm" to 6,
+        "E" to 7,
+        "F" to 8,
+        "G" to 9, "G7" to 10
     )
 
     val chords_int_tab_map: Map<String,List<Int>> = mapOf(
